632f4dddb49b5110131107f825345752ad5fa34d
songlixiao <songlixiao@songlixiao-pc>
2019-02-20 17:34:50 +0800

    增加对SpringCloud的事务支持

---
+++ src/main/java/org/dracarys/dtx/springcloud/DtxRestTemplate.java
@@ -0,0 +1,41 @@
+/**    
+ * 文件名：DtxRestTemplate.java
+ *
+ * 版本信息： V1.0
+ * 日期：2019年2月20日
+ * @author songlixiao_qd@163.com
+ * Copyright BAHEAL 2015 版权所有    
+ *     
+ */
+package org.dracarys.dtx.springcloud;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.dracarys.dtx.transaction.context.TxContextInterface;
+import org.dracarys.dtx.transaction.manager.DistributedTransactionManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * SpringCloud事务ID向下传递的工具（如需要向下层微服务传递事务，请使用此类）
+ * 
+ * @ClassName DtxRestTemplate
+ * @author songlixiao_qd@163.com
+ * @date 2019年2月20日 下午4:45:58
+ * @ModifyRemarks
+ * @version:V1.0
+ * 
+ */
+public class DtxRestTemplate extends RestTemplate {
+
+	@Override
+	protected ClientHttpRequest createRequest(URI url, HttpMethod method) throws IOException {
+		ClientHttpRequest request = super.createRequest(url, method);
+		// 将当前的TxId放入到Header中，向下传递。
+		request.getHeaders().set(TxContextInterface.KEY_RPC_TX_ID, DistributedTransactionManager.TX_ID.get());
+		return request;
+	}
+
+}
---
+++ src/main/java/org/dracarys/dtx/springcloud/SpringCloudTxContext.java
@@ -0,0 +1,46 @@
+/**    
+ * 文件名：SpringCloudTxContext.java
+ *
+ * 版本信息： V1.0
+ * 日期：2019年2月20日
+ * @author songlixiao_qd@163.com
+ * Copyright BAHEAL 2015 版权所有    
+ *     
+ */
+package org.dracarys.dtx.springcloud;
+
+import org.dracarys.dtx.transaction.context.TxContextInterface;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * SpringCloud 事务上下文
+ * 
+ * @ClassName SpringCloudTxContext
+ * @author songlixiao_qd@163.com
+ * @date 2019年2月20日 下午4:50:08
+ * @ModifyRemarks
+ * @version:V1.0
+ * 
+ */
+public class SpringCloudTxContext implements TxContextInterface {
+
+	public static ThreadLocal<Boolean> RPCTX_CALLED = new ThreadLocal<>();
+
+	@Override
+	public boolean isInRpc() {
+		return getTxIDFromContext() == null || (RPCTX_CALLED.get() != null && RPCTX_CALLED.get());
+	}
+
+	@Override
+	public String getTxIDFromContext() {
+		if (RequestContextHolder.getRequestAttributes() == null) {
+			return null;
+		}
+		if (((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest() == null) {
+			return null;
+		}
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader(KEY_RPC_TX_ID);
+	}
+
+}
