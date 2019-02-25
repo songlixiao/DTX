120144b87fed738d4f8c313aa7c8474c7d4b2807
songlixiao <songlixiao@songlixiao-pc>
2019-02-25 17:38:56 +0800

    增加SpringCloud Feign的支持

---
+++ src/main/java/org/dracarys/dtx/springcloud/DtxFeignRequestInterceptor.java
@@ -0,0 +1,26 @@
+package org.dracarys.dtx.springcloud;
+
+import org.dracarys.dtx.transaction.context.TxContextInterface;
+import org.dracarys.dtx.transaction.manager.DistributedTransactionManager;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+/**
+ * Spring Cloud 客户端用Feign时，添加调用拦截，传递TxID。
+ * 
+ * @ClassName DtxFeignRequestInterceptor
+ * @author songlixiao_qd@163.com
+ * @date 2019年2月25日 下午5:27:33
+ * @ModifyRemarks
+ * @version:V1.0
+ *
+ */
+public class DtxFeignRequestInterceptor implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate template) {
+		template.header(TxContextInterface.KEY_RPC_TX_ID, DistributedTransactionManager.TX_ID.get());
+	}
+
+}
